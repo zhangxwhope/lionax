d67547dc98abbf4928a387d8e3b3fe95df6f82a4
xiaowanzhang <zhangxiaowan@xiaowanangdeMBP.lan>
2019-11-21 22:05:42 +0800

    电脑ip

--- config/index.js
+++ config/index.js
@@ -25,6 +25,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // host: '192.168.199.178',
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
